cb2244a9d53213479510640592da3c43d5a95351
David Montufo <dmontufo@gmail.com>
2023-01-11 20:08:06 +0100

    The rest of importers and exporters

--- Assets/Scripts/DataReader.cs
+++ Assets/Scripts/DataReader.cs
@@ -22,6 +22,36 @@ public struct Attack
     public string whoAttacked;
 }
 
+public struct Death
+{
+    public Vector3 position;
+    public string whoDied;
+}
+
+public struct Hit
+{
+    public Vector3 position;
+    public string whoWasHit;
+}
+
+public struct Heal
+{
+    public Vector3 position;
+    public int currentHealth;
+}
+
+public struct Checkpoint
+{
+    public Vector3 position;
+    public int currentHealth;
+}
+
+public struct Interactuable
+{
+    public Vector3 position;
+    public string interactuableName;
+}
+
 public class DataReader : MonoBehaviour
 {
     public uint playerID = 0;
@@ -32,7 +62,11 @@ public class DataReader : MonoBehaviour
     public List<Position> positionList = new List<Position>();
     public List<Jump> jumpList = new List<Jump>();
     public List<Attack> attackList = new List<Attack>();
-
+    public List<Death> deathList = new List<Death>();
+    public List<Hit> hitList = new List<Hit>();
+    public List<Heal> healList = new List<Heal>();
+    public List<Checkpoint> checkpointList = new List<Checkpoint>();
+    public List<Interactuable> interactuableList = new List<Interactuable>();
 
     private void Awake()
     {
@@ -59,6 +93,8 @@ public class DataReader : MonoBehaviour
         StartCoroutine(GetPositions());
         StartCoroutine(GetJumps());
         StartCoroutine(GetAttacks());
+        StartCoroutine(GetDeaths());
+        StartCoroutine(GetHits());
     }
 
     IEnumerator GetPositions()
@@ -124,13 +160,116 @@ public class DataReader : MonoBehaviour
         www.Dispose();
     }
 
+    IEnumerator GetDeaths()
+    {
+        // Get deaths from bbdd
+        WWWForm form = new WWWForm();
+        form.AddField("function", "GetDeaths");
+        if (!allData) form.AddField("playerID", playerID.ToString());
+        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/Delivery3/Exporters.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            DecodePositions(www.downloadHandler.text, "deaths");
+        }
+
+        www.Dispose();
+    }
+
+    IEnumerator GetHits()
+    {
+        // Get hits from bbdd
+        WWWForm form = new WWWForm();
+        form.AddField("function", "GetHits");
+        if (!allData) form.AddField("playerID", playerID.ToString());
+        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/Delivery3/Exporters.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            DecodePositions(www.downloadHandler.text, "hits");
+        }
+
+        www.Dispose();
+    }
+
+    IEnumerator GetInteractuables()
+    {
+        // Get interactuables from bbdd
+        WWWForm form = new WWWForm();
+        form.AddField("function", "GetInteractuables");
+        if (!allData) form.AddField("playerID", playerID.ToString());
+        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/Delivery3/Exporters.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            DecodePositions(www.downloadHandler.text, "interactuables");
+        }
+
+        www.Dispose();
+    }
+
+    IEnumerator GetHeals()
+    {
+        // Get heals from bbdd
+        WWWForm form = new WWWForm();
+        form.AddField("function", "GetHeals");
+        if (!allData) form.AddField("playerID", playerID.ToString());
+        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/Delivery3/Exporters.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            DecodeHealCheckpoint(www.downloadHandler.text, "heals");
+        }
+
+        www.Dispose();
+    }
+
+    IEnumerator GetCheckpoints()
+    {
+        // Get checkpoints from bbdd
+        WWWForm form = new WWWForm();
+        form.AddField("function", "GetCheckpoints");
+        if (!allData) form.AddField("playerID", playerID.ToString());
+        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/Delivery3/Exporters.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            DecodeHealCheckpoint(www.downloadHandler.text, "checkpoints");
+        }
+
+        www.Dispose();
+    }
+
     private void DecodePositions(string text, string type)
     {
         if (text == "0 results")
             return;
         
-        List<Vector3> attacks = new List<Vector3>();
-
         string lineSeparator = "|*|";
         string valueSeparator = "|/|";
         string[] lines = text.Split(lineSeparator);
@@ -156,6 +295,49 @@ public class DataReader : MonoBehaviour
                         break;
                     case "attacks":
                         attackList.Add(new Attack() { position = new Vector3(x, y, z), whoAttacked = name });
+                        break;
+                    case "deaths":
+                        deathList.Add(new Death() { position = new Vector3(x, y, z), whoDied = name });
+                        break;
+                    case "hits":
+                        hitList.Add(new Hit() { position = new Vector3(x, y, z), whoWasHit = name });
+                        break;
+                    case "interactuables":
+                        interactuableList.Add(new Interactuable() { position = new Vector3(x, y, z), interactuableName = name });
+                        break;
+                }
+                Debug.Log("Name: " + name + " X: " + x + " Y: " + y + " Z: " + z);
+            }
+        }
+    }
+
+    private void DecodeHealCheckpoint(string text, string type)
+    {
+        if (text == "0 results")
+            return;
+
+        string lineSeparator = "|*|";
+        string valueSeparator = "|/|";
+        string[] lines = text.Split(lineSeparator);
+
+        foreach (string line in lines)
+        {
+
+            string[] values = line.Split(valueSeparator);
+            if (values.Length == 4)
+            {
+                int currentHealth = int.Parse(values[0]);
+                float x = float.Parse(values[1], System.Globalization.NumberStyles.Float, new System.Globalization.CultureInfo("en-US"));
+                float y = float.Parse(values[2], System.Globalization.NumberStyles.Float, new System.Globalization.CultureInfo("en-US"));
+                float z = float.Parse(values[3], System.Globalization.NumberStyles.Float, new System.Globalization.CultureInfo("en-US"));
+
+                switch (type)
+                {
+                    case "heals":
+                        healList.Add(new Heal() { position = new Vector3(x, y, z), currentHealth = currentHealth });
+                        break;
+                    case "checkpoints":
+                        checkpointList.Add(new Checkpoint() { position = new Vector3(x, y, z), currentHealth = currentHealth });
                         break;
                 }
                 Debug.Log("Name: " + name + " X: " + x + " Y: " + y + " Z: " + z);
--- Assets/Scripts/DataSender.cs
+++ Assets/Scripts/DataSender.cs
@@ -29,18 +29,15 @@ public class DataSender : MonoBehaviour
         NewPlayer(DateTime.Now);
     }
 
-    public void Heal()
-    {
-        
-    }
-
     static public void OnHit(Vector3 position, string whoGotHit)
     {
-        // not implemented
+        if (playerID != 0)
+            instance.SendHit(position, whoGotHit);
     }
     static public void OnDeath(Vector3 position, string whoDied)
     {
-        // not implemented
+        if (playerID != 0)
+            instance.SendDeath(position, whoDied);
     }
     static public void OnAttack(Vector3 position, string whoAttacked)
     {
@@ -55,17 +52,20 @@ public class DataSender : MonoBehaviour
     }
     static public void OnCheckpoint(string checkpointID, int currentHealth)
     {
-        // not implemented
+        if (playerID != 0)
+            instance.SendCheckpoint(checkpointID, currentHealth);
     }
 
     static public void OnHeal(Vector3 position, int currentHealth)
     {
-        // not implemented
+        if (playerID != 0)
+            instance.SendHeal(position, currentHealth);
     }
 
     static public void OnInteractuable(Vector3 position, string interactuableName)
     {
-        // not implemented
+        if (playerID != 0)
+            instance.SendInteractuable(position, interactuableName);
     }
 
     static public void SendPosition(Vector3 position, string name)
@@ -182,6 +182,166 @@ public class DataSender : MonoBehaviour
         form.AddField("y", position.y.ToString());
         form.AddField("z", position.z.ToString());
         form.AddField("name", name);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+            }
+        }
+    }
+    #endregion
+
+    #region Death
+    void SendDeath(Vector3 position, string name)
+    {
+        StartCoroutine(SendNewDeath(position, name));
+    }
+
+    IEnumerator SendNewDeath(Vector3 position, string name)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("function", "NewDeath");
+        form.AddField("dateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        form.AddField("playerID", playerID);
+        form.AddField("x", position.x.ToString());
+        form.AddField("y", position.y.ToString());
+        form.AddField("z", position.z.ToString());
+        form.AddField("name", name);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+            }
+        }
+    }
+    #endregion
+
+    #region Hit
+    void SendHit(Vector3 position, string name)
+    {
+        StartCoroutine(SendNewHit(position, name));
+    }
+
+    IEnumerator SendNewHit(Vector3 position, string name)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("function", "NewHit");
+        form.AddField("dateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        form.AddField("playerID", playerID);
+        form.AddField("x", position.x.ToString());
+        form.AddField("y", position.y.ToString());
+        form.AddField("z", position.z.ToString());
+        form.AddField("name", name);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+            }
+        }
+    }
+    #endregion
+
+    #region Heal
+    void SendHeal(Vector3 position, int currentHealth)
+    {
+        StartCoroutine(SendNewHeal(position, currentHealth));
+    }
+
+    IEnumerator SendNewHeal(Vector3 position, int currentHealth)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("function", "NewHeal");
+        form.AddField("dateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        form.AddField("playerID", playerID);
+        form.AddField("x", position.x.ToString());
+        form.AddField("y", position.y.ToString());
+        form.AddField("z", position.z.ToString());
+        form.AddField("currentHealth", currentHealth);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+            }
+        }
+    }
+    #endregion
+
+    #region Checkpoint
+    void SendCheckpoint(Vector3 position, int currentHealth)
+    {
+        StartCoroutine(SendNewCheckpoint(position, currentHealth));
+    }
+
+    IEnumerator SendNewCheckpoint(Vector3 position, int currentHealth)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("function", "NewCheckpoint");
+        form.AddField("dateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        form.AddField("playerID", playerID);
+        form.AddField("x", position.x.ToString());
+        form.AddField("y", position.y.ToString());
+        form.AddField("z", position.z.ToString());
+        form.AddField("currentHealth", currentHealth);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+            }
+        }
+    }
+    #endregion
+
+    #region Interactuable
+    void SendInteractuable(Vector3 position, string interactuableName)
+    {
+        StartCoroutine(SendNewInteractuable(position, interactuableName));
+    }
+
+    IEnumerator SendNewInteractuable(Vector3 position, string interactuableName)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("function", "NewInteractuable");
+        form.AddField("dateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        form.AddField("playerID", playerID);
+        form.AddField("x", position.x.ToString());
+        form.AddField("y", position.y.ToString());
+        form.AddField("z", position.z.ToString());
+        form.AddField("interactuableName", interactuableName);
 
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
